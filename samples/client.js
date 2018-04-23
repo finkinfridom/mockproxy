c6f87e1962eb16ae9952de444b99b930b121672d
alberto.maghini <alberto.maghini@deltatre.com>
2018-04-22 17:30:46 -0700

    created client sample. managed 200/500/404 statusCode. managed response content-type

---
+++ samples/client.js
@@ -0,0 +1,34 @@
+const dotenv = require('dotenv');
+dotenv.load();
+const debug = require("debug")("mockproxy:sampleclient");
+const request = require('request');
+debug('starting request to ::' + process.env.SERVER_PORT);
+request({
+    url: 'http://www.smashingmagazine.com/',
+    headers: {
+        //sample value to test workflow
+        mp_key: 'f2012b10a7b0434885a1db754d74fb09'
+    },
+    proxy: "http://localhost:" + process.env.SERVER_PORT
+}, (error, response, body) => {
+    debug('URL', 'http://www.smashingmagazine.com/');
+    debug(`statusCode: ${response.statusCode}`);
+    debug(`content-type: ${response.headers['content-type']}`);
+    debug(`all headers: ${JSON.stringify(response.headers)}`);
+    // debug('body', body);
+});
+
+request({
+    url: 'http://non.existing.url/',
+    headers: {
+        //sample value to test workflow
+        mp_key: 'f2012b10a7b0434885a1db754d74fb09'
+    },
+    proxy: "http://localhost:" + process.env.SERVER_PORT
+}, (error, response, body) => {
+    debug('URL', 'http://non.existing.url/');
+    debug(`statusCode: ${response.statusCode}`);
+    debug(`content-type: ${response.headers['content-type']}`);
+    debug(`all headers: ${JSON.stringify(response.headers)}`);
+
+});
---
+++ server/libs/storage.js
@@ -0,0 +1,26 @@
+const debug = require("debug")("mockproxy:storage");
+const _storage = {};
+const storageFactory = {
+    create: function () {
+
+        //init storage. temporarily in-memory
+        return this;
+    },
+    get: function (key) {
+        if (!key) {
+            return void 0;
+        }
+        return _storage[key];
+    },
+    getSecret: function (authKey, url, headers) {
+        return `${authKey}_${url}_${JSON.stringify(headers)}`;
+    },
+    save: function (key, value) {
+        if (!key) {
+            return false;
+        }
+        _storage[key] = value;
+        return true;
+    }
+};
+module.exports = storageFactory;
